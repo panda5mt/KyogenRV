fe13cec489aa3573e6d4e81a750bfa3566d5a6b7
lynxeyed <lynxeyed@live.jp>
2020-04-30 00:10:03 +0900

    fixed indent

--- src/main/scala/mem/imem.scala
+++ src/main/scala/mem/imem.scala
@@ -1,3 +1,4 @@
+// See README.md for license details.
 package mem
 
 import chisel3._
@@ -11,24 +12,24 @@ class IMem extends Module {
 
     // initialization
     //val mem     = SyncReadMem(1024, UInt(32.W))
-    val mem     = Mem(1024, UInt(32.W))
+    val mem: Mem[UInt] = Mem(1024, UInt(32.W))
 
-    val i_ack   = RegInit(true.B)
-    val i_req   = RegInit(false.B)
+    val i_ack: Bool = RegInit(true.B)
+    val i_req: Bool = RegInit(false.B)
     //val i_data  = RegInit(0.U(32.W))
     
-    val w_ack   = RegInit(true.B)
+    val w_ack: Bool = RegInit(true.B)
     
     // read operation
-    i_req           :=io.r_ach.req 
+    i_req           :=io.r_ach.req
     io.r_dch.data   := mem.read(io.r_ach.addr)
-    i_ack           := i_req   
+    i_ack           := i_req
     io.r_dch.ack    := i_ack
 
     // write operation
     when(io.w_ach.req === true.B) {
         mem.write(io.w_ach.addr,io.w_dch.data)
-        //w_ack := true.B    
+        //w_ack := true.B
     }
 
     io.w_dch.ack    := w_ack
