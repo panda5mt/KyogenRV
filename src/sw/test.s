571b7e5ba10290df5309f0903ade9a0f50c2c5dc
lynxeyed <lynxeyed@live.jp>
2020-05-25 16:57:35 +0900

    update ALU function

--- src/sw/test.s
+++ src/sw/test.s
@@ -3,7 +3,7 @@
 _label1:
     addi x13,  x0, 3        # x13 = 3
     addi x14,  x0, 3        # x14 = 3
-    beq  x13, x14, _label2  # if(x13 == x14) jump to label6
+    bne  x13, x14, _label2  # if(x13 == x14) jump to label6
     addi x15,  x0, 0xAA
     addi x16,  x0, 0xBB
 
@@ -16,7 +16,7 @@ _label3:
 
 _label4:
     addi x2,  x0, 2
-    jalr x3,  x1, 0         # x3 = label5, jump label3
+    jalr x3,  x1, -4         # x3 = label5, jump label3
 
 _label5:
     nop
