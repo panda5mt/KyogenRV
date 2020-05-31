0e1e5cec240c8f4cb658895b168caf64785e8338
lynxeyed <lynxeyed@live.jp>
2020-05-31 21:56:13 +0900

    fixed assemble file

--- src/sw/test.s
+++ src/sw/test.s
@@ -2,21 +2,21 @@
 _label1:
         lui     x1,  0x08           # x1 = 0x08 << 12
         addi    x1,  x1, 0x04       # x1 = x1 + 0x04 = 0x8004
-        addi    x2,  x1, 0x00      #  x2 = x1 (WBステージに反映されるのを待てない
+        addi    x2,  x1, 0x00      #  x2 = x1
 
-        sw      x2, 12(x31)        #  EXステージで ↑ のx2を取り込まないといけない
-        lw      x3, 12(x31)        #  x3 => 0x8004になっているべき
+        sw      x2, 12(x31)        #  dmem[x31 + 12] = x2 = 0x8004
+        lw      x3, 12(x31)        #  x3 = dmem[x31 + 12] => 0x8004になっているべき
 
 
         addi    x4, x0, 0xAA       # x4 = 0xAA
         sw      x4, 12(x31)        # dmem[x31 + 12] = x4 = 0xAA
-        lw      x5, 12(x31)        # x5 = 0xAA
+        lw      x5, 12(x31)        # x5 = dmem[x31 + 12] = 0xAA
 
-        beq    x4, x5, _label2
+        beq    x4, x5, _label2　　　# x4 = x5 なら_label2へジャンプ
         addi    x10, x0, 0x44
         addi    x11, x0, 0x55
 
 
 
  _label2:
-        jal x0, _label2
+        jal x0, _label2             # 無限ループ
