b7dba475851921fa2c02f64efa09ca245255cd48
lynxeyed <lynxeyed@live.jp>
2020-06-02 22:45:02 +0900

    update test code

--- src/sw/test.s
+++ src/sw/test.s
@@ -2,18 +2,18 @@
 _label1:
         lui     x1,  0x08           # x1 = 0x08 << 12
         addi    x1,  x1, 0x04       # x1 = x1 + 0x04 = 0x8004
-        addi    x2,  x1, 0x00      #  x2 = x1
+        addi    x2,  x1, 0x00       #  x2 = x1
 
-        sw      x2, 12(x31)        #  dmem[x31 + 12] = x2 = 0x8004
-        lw      x3, 12(x31)        #  x3 = dmem[x31 + 12] => 0x8004になっているべき
-
-
-        addi    x4, x0, 0xAA       # x4 = 0xAA
-        sw      x4, 12(x31)        # dmem[x31 + 12] = x4 = 0xAA
-        lw      x5, 12(x31)        # x5 = dmem[x31 + 12] = 0xAA
+        sw      x2, 12(x31)         #  dmem[x31 + 12] = x2 = 0x8004
+        lw      x3, 12(x31)         #  x3 = dmem[x31 + 12] => 0x8004になっているべき
 
 
-        beq     x4, x5,  _label2   # x4 = x5 なら_label2へジャンプ
+        addi    x4, x0, 0xAA        # x4 = 0xAA
+        sw      x4, 12(x31)         # dmem[x31 + 12] = x4 = 0xAA
+        lw      x5, 12(x31)         # x5 = dmem[x31 + 12] = 0xAA
+
+
+        beq     x4, x5,  _label2    # x4 = x5 なら_label2へジャンプ
         addi    x10, x0, 0x44
         addi    x11, x0, 0x55
 
