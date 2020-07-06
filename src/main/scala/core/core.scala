9c9a558f29ad5dcc8075bd33c3ba507492ff302d
lynxeyed <lynxeyed@live.jp>
2020-07-06 11:27:48 +0900

    fixed sample file

--- src/main/scala/core/core.scala
+++ src/main/scala/core/core.scala
@@ -578,7 +578,7 @@ object Test extends App {
     iotesters.Driver.execute(args, () => new CpuBus())(testerGen = c => {
         new PeekPokeTester(c) {
             // read from binary file
-            val s: BufferedSource = Source.fromFile("src/sw/test5.hex")
+            val s: BufferedSource = Source.fromFile("src/sw/test.hex")
             var buffs: Array[String] = _
             try {
                 buffs = s.getLines.toArray
