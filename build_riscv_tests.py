642f1cd6d1a8d726d0138f4f0c9531f3da6af556
lynxeyed <lynxeyed@live.jp>
2020-07-13 14:47:06 +0900

    added comment

--- build_riscv_tests.py
+++ build_riscv_tests.py
@@ -9,19 +9,18 @@ dir_path = os.getcwd() +'/tests/share/riscv-tests/isa/'
 proc = subprocess.run('rm -rf *.bin *.hex', shell=True, text=True, cwd=dir_path)
 files = [path for path in os.listdir(dir_path)]
 
-files_in = [s for s in files if 'rv32ui' in s]
-files_in2 = [s for s in files if 'rv32mi' in s]
+files_in = [s for s in files if 'rv32ui' in s]  # select rv32ui*.* file
+files_in2 = [s for s in files if 'rv32mi' in s] # select rv32mi*.* file
 files_in.extend(files_in2)
-files_in = [s for s in files_in if '.dump' not in s]
+files_in = [s for s in files_in if '.dump' not in s] # exclude *.dump file
 # files_in = [rv32mi* and rv32ui* and not in *.dump]
 
 
 for item in files_in:
-
     str1='riscv64-unknown-elf-objcopy --gap-fill 0 -O binary '+item+' '+item+".bin"
-    if platform.system() == 'Darwin': #MacOS?
+    if platform.system() == 'Darwin': # macOS?
         str2 = 'god -An -v -tx4 -w4 '+item+'.bin > '+item+'.hex'
-    else:   #Linux or WSL?
+    else:   # Linux or WSL?
         str2 = 'od -An -v -tx4 -w4 '+item+'.bin > '+item+'.hex'
     proc = subprocess.run(str1, shell=True, text=True, cwd=dir_path)
     proc = subprocess.run(str2, shell=True, text=True, cwd=dir_path)
