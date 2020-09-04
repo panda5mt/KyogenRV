5fbfe78b1d23efe289132547a1f4af21cc83fd73
lynxeyed <lynxeyed@live.jp>
2020-09-04 21:30:00 +0900

    added flash logic when waitrequest.

--- src/main/scala/core/core.scala
+++ src/main/scala/core/core.scala
@@ -132,7 +132,7 @@ class KyogenRVCpu extends Module {
         if_npc := npc_ini
         io.r_imem_dat.req := imem_read_sig
         valid_imem := false.B
-    }.elsewhen(waitrequest) {
+    }.elsewhen(io.sw.w_waitrequest_sig) {
         if_pc := pc_ini
         if_npc := npc_ini
         io.r_imem_dat.req := false.B
