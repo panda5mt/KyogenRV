4da9435282148b945a997873cf5ea1bf4efff783
MINIAMD\lynxe <lynxeyed@live.jp>
2020-12-17 15:55:30 +0900

    added sample 'hello world'

---
+++ src/sw/helloworld.s
@@ -0,0 +1,45 @@
+    lui     x1, 0x08
+    addi    x1, x1, 0x20
+
+    li      x2, 'h'
+    jal     x20, _send
+    li      x2, 'e'
+    jal     x20, _send
+    li      x2, 'l'
+    jal     x20, _send
+    li      x2, 'l'
+    jal     x20, _send
+    li      x2, 'o'
+    jal     x20, _send
+    li      x2, ' '
+    jal     x20, _send
+    li      x2, 'w'
+    jal     x20, _send
+    li      x2, 'o'
+    jal     x20, _send
+    li      x2, 'r'
+    jal     x20, _send
+    li      x2, 'l'
+    jal     x20, _send
+    li      x2, 'd'
+    jal     x20, _send
+    li      x2, '\r'
+    jal     x20, _send
+    li      x2, '\n'
+    jal     x20, _send
+
+_loop0:
+    jal     x0, _loop0
+
+_send:
+
+    nop
+_check_status:
+    lw      x3, 8(x1) # x3 = status register
+    andi    x3,x3,0x20
+    beq     zero, x3, _check_status
+    sw      x2, 4(x1)
+    jalr    x0,  0(x20)
+
+
+
