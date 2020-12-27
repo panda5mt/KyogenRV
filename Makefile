6935430761a2f78b37e566654a103cda1ca018a3
lynxeyed <lynxeyed@live.jp>
2020-12-27 16:26:02 +0900

    fixed C code

--- Makefile
+++ Makefile
@@ -5,17 +5,20 @@ ASM_TARGET = test
 ASM_DIR = src/sw
 
 include  src/sw/common.mk
+include  src/sw/common2.mk
 .DEFAULT_GOAL := test
 # Generate Verilog code
 hdl:
 	$(SBT) 'runMain core.kyogenrv --target-dir $(TARGET)'
 test: $(ASM_DIR)/$(ASM_TARGET).hex
 	$(SBT) 'runMain core.Test'
-clean:
+clean: c_clean
 	rm -rf $(TARGET)/*.json $(TARGET)/*.fir $(TARGET)/*.v $(TARGET)/*.hex $(ASM_DIR)/$(ASM_TARGET).elf $(ASM_DIR)/$(ASM_TARGET).bin $(ASM_DIR)/*.hex $(ASM_DIR)/*.log
+
 riscv-tests:
 	python3 build_riscv_tests.py
 	$(SBT) 'test:testOnly core.TestCoreAll'
 tester-gen:
 	python3 build_riscv_tests.py
+
 
---
+++ src/sw/common2.mk
@@ -0,0 +1,33 @@
+RISCVGNU = riscv32-unknown-elf
+
+AOPS = -march=rv32i -mabi=ilp32
+COPS = -march=rv32i -mabi=ilp32 -Wall -O0
+#-g  -nostdlib -nostartfiles -ffreestanding
+#COPS = -march=rv32imac -Wall -O2  -nostdlib -nostartfiles -ffreestanding
+c_all : $(ASM_DIR)/blinker.hex
+
+c_clean :
+	rm -f *.elf
+	rm -f *.bin
+	rm -f *.hex
+	rm -f *.list
+
+$(ASM_DIR)/utils.o : $(ASM_DIR)/utils.S
+	$(RISCVGNU)-as $(AOPS) $(ASM_DIR)/utils.S -o $(ASM_DIR)/utils.o
+
+$(ASM_DIR)/led.o : $(ASM_DIR)/led.c
+	$(RISCVGNU)-gcc $(COPS) -c $(ASM_DIR)/led.c -o $(ASM_DIR)/led.o
+
+$(ASM_DIR)/blinker.elf : $(ASM_DIR)/linker.ld $(ASM_DIR)/utils.o $(ASM_DIR)/led.o
+	$(RISCVGNU)-ld utils.o led.o -T $(ASM_DIR)/linker.ld -o $(ASM_DIR)/blinker.elf
+
+$(ASM_DIR)/blinker.bin: $(ASM_DIR)/blinker.elf
+	$(RISCVGNU)-objcopy --gap-fill 0 -O binary $(ASM_DIR)/blinker.elf $(ASM_DIR)/blinker.bin
+
+
+$(ASM_DIR)/blinker.hex: $(ASM_DIR)/blinker.bin
+ifeq  ($(shell uname),Darwin)
+	god -An -v -tx4 -w4 $(ASM_DIR)/blinker.bin > $(ASM_DIR)/blinker.hex
+else
+	od -An -v -tx4 -w4 $(ASM_DIR)/blinker.bin > $(ASM_DIR)/blinker.hex
+endif
