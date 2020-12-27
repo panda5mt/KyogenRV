b70c7395bdb7d19183a5148182d3023b3d8f97b4
lynxeyed <lynxeyed@live.jp>
2020-12-27 16:35:50 +0900

    fixed Makefile

--- src/sw/common2.mk
+++ src/sw/common2.mk
@@ -7,10 +7,11 @@ COPS = -march=rv32i -mabi=ilp32 -Wall -O0
 c_all : $(ASM_DIR)/blinker.hex
 
 c_clean :
-	rm -f *.elf
-	rm -f *.bin
-	rm -f *.hex
-	rm -f *.list
+	rm -f $(ASM_DIR)/*.elf
+	rm -f $(ASM_DIR)/*.bin
+	rm -f $(ASM_DIR)/*.list
+	rm -f $(ASM_DIR)/*.o
+#	rm -f $(ASM_DIR)/*.hex
 
 $(ASM_DIR)/utils.o : $(ASM_DIR)/utils.S
 	$(RISCVGNU)-as $(AOPS) $(ASM_DIR)/utils.S -o $(ASM_DIR)/utils.o
@@ -19,7 +20,7 @@ $(ASM_DIR)/led.o : $(ASM_DIR)/led.c
 	$(RISCVGNU)-gcc $(COPS) -c $(ASM_DIR)/led.c -o $(ASM_DIR)/led.o
 
 $(ASM_DIR)/blinker.elf : $(ASM_DIR)/linker.ld $(ASM_DIR)/utils.o $(ASM_DIR)/led.o
-	$(RISCVGNU)-ld utils.o led.o -T $(ASM_DIR)/linker.ld -o $(ASM_DIR)/blinker.elf
+	$(RISCVGNU)-ld $(ASM_DIR)/utils.o $(ASM_DIR)/led.o -T $(ASM_DIR)/linker.ld -o $(ASM_DIR)/blinker.elf
 
 $(ASM_DIR)/blinker.bin: $(ASM_DIR)/blinker.elf
 	$(RISCVGNU)-objcopy --gap-fill 0 -O binary $(ASM_DIR)/blinker.elf $(ASM_DIR)/blinker.bin
