a03a564e34d0fc433c69633d927d383f0f224911
MINIAMD\lynxe <lynxeyed@live.jp>
2020-12-28 16:05:27 +0900

    update test code

---
+++ src/sw/test_interrupt.s
@@ -0,0 +1,34 @@
+_label0:
+        addi    x1, x0, 0x18        # <0x00>
+        csrrw   x0, mtvec, x1       # <0x04> : mtvec = <0x18>
+        lui     x1, 0x01
+        srli    x1, x1, 1
+        csrrw   x0, mie, x1         # enable external interrupt(mie.mtie = 1)
+        jal     x0, _label1         # jump _label1
+
+_label_expc:
+        addi    x2, x0, 0xAA        # <0x18>
+        csrr    x3, mepc            #
+        csrr    x4, mcause          #
+        jal     x0, _label_expc     # <0x24> : loop
+
+_label1:
+        addi    x5,  x0, 0xBB       # <0x28>
+_label2:
+        addi    x6,  x0, 0xCC       # <0x2C>
+        jal     x0, _label2         # <0x30>
+
+
+
+
+
+#    lui     x1, 0x08
+#    li      x2, 0xAA
+#    sw      x2, 0(x1)
+#    lw      x3, 0(x1)
+#    addi    x3, x3, 1
+#    sw      x3, 0(x1)
+#_loop1:
+#    jal     x0, _loop1
+
+
