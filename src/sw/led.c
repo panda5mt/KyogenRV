5680cc2ff73299bb0a8a854ed16525537f3dc43d
MINIAMD\lynxe <lynxeyed@live.jp>
2021-01-16 22:30:32 +0900

    fixed linker

--- src/sw/led.c
+++ src/sw/led.c
@@ -5,13 +5,12 @@ void dummy (void);
 
 int main(int argc, char *argv[])
 {
-    unsigned int rx;
+    volatile unsigned int rx;
 
     while (1) {
-     put32(GPIO_BASE, 0xAA);
-        for (rx = 0; rx < 600000; rx++) dummy();
+        for (rx = 0; rx < 1200000; rx++) dummy();
         put32(GPIO_BASE, 0x55);
-        for (rx = 0; rx < 600000; rx++) dummy();
+        for (rx = 0; rx < 1200000; rx++) dummy();
         put32(GPIO_BASE, 0xAA);
     }
     return 0;
