f5ece79b4a8f7a397ab93341ea3e72d878d219f4
MINIAMD\lynxe <lynxeyed@live.jp>
2021-01-18 14:45:21 +0900

    fixed sample code.

--- src/sw/led.c
+++ src/sw/led.c
@@ -7,12 +7,15 @@ void dummy (void);
 
 #define GPIO_BASE         0x8000
 
+// wait msec counter
 void wait_ms(unsigned int msec){
     volatile unsigned int oldtime;
     oldtime = get_timel();
     while((get_timel()-oldtime) < 60000 * msec); //1msec
-
 }
+
+
+// main function
 int main(int argc, char *argv[]) {
     while (1) {
         wait_ms(500);
