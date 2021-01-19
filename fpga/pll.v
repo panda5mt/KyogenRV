748b8e07736e7d52a85c2512610d0b53a5aa9e1c
MINIAMD\lynxe <lynxeyed@live.jp>
2021-01-19 11:23:01 +0900

    fixed frequency.(C and PD project)

--- fpga/pll.v
+++ fpga/pll.v
@@ -95,9 +95,9 @@ module pll (
 				.vcounderrange ());
 	defparam
 		altpll_component.bandwidth_type = "AUTO",
-		altpll_component.clk0_divide_by = 2,
+		altpll_component.clk0_divide_by = 12,
 		altpll_component.clk0_duty_cycle = 50,
-		altpll_component.clk0_multiply_by = 5,
+		altpll_component.clk0_multiply_by = 35,
 		altpll_component.clk0_phase_shift = "0",
 		altpll_component.compensate_clock = "CLK0",
 		altpll_component.inclk0_input_frequency = 41666,
@@ -174,7 +174,7 @@ endmodule
 // Retrieval info: PRIVATE: DEVICE_SPEED_GRADE STRING "Any"
 // Retrieval info: PRIVATE: DIV_FACTOR0 NUMERIC "1"
 // Retrieval info: PRIVATE: DUTY_CYCLE0 STRING "50.00000000"
-// Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "60.000000"
+// Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "70.000000"
 // Retrieval info: PRIVATE: EXPLICIT_SWITCHOVER_COUNTER STRING "0"
 // Retrieval info: PRIVATE: EXT_FEEDBACK_RADIO STRING "0"
 // Retrieval info: PRIVATE: GLOCKED_COUNTER_EDIT_CHANGED STRING "1"
@@ -199,7 +199,7 @@ endmodule
 // Retrieval info: PRIVATE: MIRROR_CLK0 STRING "0"
 // Retrieval info: PRIVATE: MULT_FACTOR0 NUMERIC "1"
 // Retrieval info: PRIVATE: NORMAL_MODE_RADIO STRING "1"
-// Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "60.00000000"
+// Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "70.00000000"
 // Retrieval info: PRIVATE: OUTPUT_FREQ_MODE0 STRING "1"
 // Retrieval info: PRIVATE: OUTPUT_FREQ_UNIT0 STRING "MHz"
 // Retrieval info: PRIVATE: PHASE_RECONFIG_FEATURE_ENABLED STRING "1"
@@ -238,9 +238,9 @@ endmodule
 // Retrieval info: PRIVATE: ZERO_DELAY_RADIO STRING "0"
 // Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 // Retrieval info: CONSTANT: BANDWIDTH_TYPE STRING "AUTO"
-// Retrieval info: CONSTANT: CLK0_DIVIDE_BY NUMERIC "2"
+// Retrieval info: CONSTANT: CLK0_DIVIDE_BY NUMERIC "12"
 // Retrieval info: CONSTANT: CLK0_DUTY_CYCLE NUMERIC "50"
-// Retrieval info: CONSTANT: CLK0_MULTIPLY_BY NUMERIC "5"
+// Retrieval info: CONSTANT: CLK0_MULTIPLY_BY NUMERIC "35"
 // Retrieval info: CONSTANT: CLK0_PHASE_SHIFT STRING "0"
 // Retrieval info: CONSTANT: COMPENSATE_CLOCK STRING "CLK0"
 // Retrieval info: CONSTANT: INCLK0_INPUT_FREQUENCY NUMERIC "41666"
--- src/sw/led.c
+++ src/sw/led.c
@@ -1,3 +1,6 @@
+#include <newlib.h>
+#include <math.h>
+
 void put32(unsigned int, unsigned int);
 unsigned int get32(unsigned int);
 unsigned int get_timel(void);
@@ -6,23 +9,23 @@ unsigned int get_timeh(void);
 void dummy (void);
 
 #define GPIO_BASE       0x8000
-#define XTAL_FREQ_KHZ   60000
+#define XTAL_FREQ_KHZ   70000 // 70000kHz = 70MHz
 
 // wait msec counter
-void wait_ms(unsigned int msec){
+void wait_ms(unsigned int msec) {
     volatile unsigned int oldtime;
     oldtime = get_timel();
-    while((get_timel()-oldtime) < XTAL_FREQ_KHZ * msec); //1msec
-}
+    while((get_timel()-oldtime) < XTAL_FREQ_KHZ * msec);
+ }
 
 
 // main function
 int main(int argc, char *argv[]) {
     while (1) {
-        wait_ms(500);
+        wait_ms(1000);
         put32(GPIO_BASE, 0x55);
 
-        wait_ms(500);
+        wait_ms(1000);
         put32(GPIO_BASE, 0xAA);
     }
     return 0;
